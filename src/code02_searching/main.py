40cf7b73fd422fba3930e828ca9756236ded8085
Faiz Akbar <faizzakbar385@gmail.com>
2026-09-21 12:03:09 +0700

    sudah diupdate

--- src/code02_searching/main.py
+++ src/code02_searching/main.py
@@ -21,7 +21,7 @@ def main():
     print("\nMovie English:")
     english_movies = collection.find_by_language("en")
 
-    for movie in english_movies[:5]:
+    for movie in english_movies:
         print(movie)
 
     # Film dengan rating tertinggi
